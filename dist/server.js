c049189900f52c74b81b640031e1ccf9259bc8d8
MUGABE Nshuti Ignace <ignacemugabe@gmail.com>
2025-10-02 09:52:12 +0200

    fix: deployment

--- dist/server.js
+++ dist/server.js
@@ -1,7 +1,9 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const ts_dotenv_1 = require("ts-dotenv");
@@ -10,26 +12,25 @@ const cors_1 = __importDefault(require("cors"));
 const indexRouters_1 = __importDefault(require("./routes/indexRouters"));
 const swagger_1 = require("./swagger");
 const env = (0, ts_dotenv_1.load)({
-    DB_PORT: Number,
-    DB_USER: String,
-    DB_PASSWORD: String,
-    MONGO_DB_URI: String,
-    DB_NAME: String,
+  DB_PORT: Number,
+  MONGO_DB_URI: String,
 });
 const port = env.DB_PORT || 7000;
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
 const allowedOrigin = "http://localhost:8080";
-app.use((0, cors_1.default)({
+app.use(
+  (0, cors_1.default)({
     origin: allowedOrigin,
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 // Swagger docs
 app.use("/api-docs", swagger_1.swaggerUiHandler, swagger_1.swaggerUiSetup);
 // api_v1 routes
-app.use('/api_v1', indexRouters_1.default);
+app.use("/api_v1", indexRouters_1.default);
 (0, database_1.connectDB)();
 app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
-    console.log(`Swagger docs available at http://localhost:${port}/api-docs`);
+  console.log(`Server is running on ${port}`);
+  console.log(`Swagger docs available at ${port}/api-docs`);
 });
